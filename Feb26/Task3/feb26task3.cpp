bd83b67b67a9ec9d54f0443a2db330afbae98083
Demonical <d3m0n5c1l@d3m0n5c1l.local>
2025-02-26 15:31:52 +0300

    Add Feb26Task3

--- Feb26/Task3/feb26task3.cpp
+++ Feb26/Task3/feb26task3.cpp
@@ -1,3 +1,38 @@
-//
-// Created by d3m0n5c1l on 26/02/2025.
-//
+#include <iostream>
+#include <vector>
+using namespace std;
+
+template <typename T>
+std::vector<T> map(const std::vector<T>& vec, int (*func)(T)) {
+    std::vector<T> result;
+    result.reserve(vec.size());
+    for (const auto& element : vec) {
+        result.push_back(func(element));
+    }
+    return result;
+}
+
+template <typename T>
+std::vector<T> filter(const std::vector<T>& vec, std::function<bool(T)> predicate) {
+    std::vector<T> result;
+    for (const auto& element : vec) {
+        if (predicate(element)) {
+            result.push_back(element);
+        }
+    }
+    return result;
+}
+
+int main() {
+    vector<int> nums = {1, 2, 3, 4, 5};
+    auto squared = map<int>(nums, [](int a) { return a * a; });
+    auto odds = filter<int>(nums, [](int a) { return a % 2; });
+    for (const auto& element : squared) {
+        cout << element << " ";
+    }
+    cout << endl;
+    for (const auto& element : odds) {
+        cout << element << " ";
+    }
+    return 0;
+}
